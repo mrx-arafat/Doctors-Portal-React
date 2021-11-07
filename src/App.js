ec92afde3b055a90bf5385af52b1d1448c508127
Easin Arafat <arafatmrx@gmail.com>
2021-11-08 00:02:53 +0600

    appointment banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Navigation from "./Shared/Navigation/Navigation";
 import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navigation></Navigation>
       <BrowserRouter>
         <Switch>
           <Route path="/home">
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 
 import Grid from "@mui/material/Grid";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 import fluoride from "../../../images/fluoride.png";
 import cavity from "../../../images/cavity.png";
@@ -29,18 +29,27 @@ const services = [
     img: whitening,
   },
 ];
-
 const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+        <Typography
+          sx={{ fontWeight: 500, m: 2, color: "#5CE7ED" }}
+          variant="h6"
+          component="div"
+        >
+          OUR SERVICES
+        </Typography>
+        <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
+          Services We Provide
+        </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
           {services.map((service) => (
-            <Service service={service}></Service>
+            <Service key={service.name} service={service}></Service>
           ))}
         </Grid>
       </Container>
