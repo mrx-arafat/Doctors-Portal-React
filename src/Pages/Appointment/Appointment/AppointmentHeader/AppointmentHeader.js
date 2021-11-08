6b7728d5503fb937b54ebc25a548d5b787f8c624
Easin Arafat <arafatmrx@gmail.com>
2021-11-08 12:49:19 +0600

    date fns done

--- src/Pages/Appointment/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,9 +1,21 @@
+import { Container, Grid } from "@mui/material";
+import chair from "../../../../images/chair.png";
 import React from "react";
+import Calender from "../../../../Shared/Calender/Calender";
 
 const AppointmentHeader = () => {
   return (
     <div>
       <h3>appointment header </h3>
+
+      <Container>
+        <Grid xs={12} md={6}>
+          <Calender></Calender>
+        </Grid>
+        <Grid xs={12} md={6}>
+          <img style={{ width: 400, marginTop: "-110px" }} src={chair} alt="" />
+        </Grid>
+      </Container>
     </div>
   );
 };
---
+++ src/Shared/Calender/Calender.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import isWeekend from "date-fns/isWeekend";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import StaticDatePicker from "@mui/lab/StaticDatePicker";
+const Calender = () => {
+  return (
+    <div>
+      <h3>hello from Calender</h3>
+    </div>
+  );
+};
+
+export default Calender;
