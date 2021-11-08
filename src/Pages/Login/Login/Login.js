c34b0883afde5e0debb9aadca0f48505906a47db
Easin Arafat <arafatmrx@gmail.com>
2021-11-09 00:59:12 +0600

    login page design

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,13 +1,56 @@
-import { Container, Grid } from "@mui/material";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 import login from "../../../images/login.png";
 
 const Login = () => {
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Container style={{ display: "flex" }}>
-      <Grid xs={8} md={6}></Grid>
-      <Grid xs={8} md={6}>
-        <img src={login} alt="" />
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+          <Typography
+            sx={{ fontWeight: "bold", fontSize: "h6.fontSize" }}
+            variant="body1"
+            gutterBottom
+          >
+            Login
+          </Typography>
+          <form onSubmit={handleLoginSubmit}>
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              label="Your Email"
+              name="email"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              label="Your Password"
+              type="password"
+              name="password"
+              variant="standard"
+            />
+
+            <Button
+              sx={{ width: "75%", m: 1 }}
+              type="submit"
+              variant="contained"
+            >
+              Login
+            </Button>
+            <NavLink style={{ textDecoration: "none" }} to="/register">
+              <Button variant="text">New User? Please Register</Button>
+            </NavLink>
+          </form>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img style={{ width: "100%" }} src={login} alt="" />
+        </Grid>
       </Grid>
     </Container>
   );
